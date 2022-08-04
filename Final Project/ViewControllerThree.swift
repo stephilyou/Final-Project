0ef7de532a6836a63338436197a10e1121e5043e
Stephi Lyou <stephilyou@hotmail.com>
2022-08-04 14:09:33 -0400

    finalized project

--- Final Project/ViewControllerThree.swift
+++ Final Project/ViewControllerThree.swift
@@ -9,7 +9,17 @@ import UIKit
 
 class ViewControllerThree: UIViewController {
 
+    @IBOutlet var cestdButton: UIButton!
     @IBOutlet var alliHallaButton: UIButton!
+    @IBOutlet var arqButton: UIButton!
+    @IBOutlet var alphineButton: UIButton!
+    @IBOutlet var loudButton: UIButton!
+    @IBOutlet var goodButton: UIButton!
+    @IBOutlet var copperButton: UIButton!
+    @IBOutlet var curveButton: UIButton!
+    @IBOutlet var altarButton: UIButton!
+    @IBOutlet var christianButton: UIButton!
+    @IBOutlet var eshaktiButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,6 +29,56 @@ class ViewControllerThree: UIViewController {
     @IBAction func alliHallaButton(_ sender: Any) {
         let url = URL (string: "https://www.allihalla.com/")!
         UIApplication.shared.open (url)
+    }
+    
+    @IBAction func cestdButton(_ sender: Any) {
+        let url1 = URL (string: "https://www.cestd.com")!
+        UIApplication.shared.open (url1)
+    }
+    
+    @IBAction func eshaktiButton(_ sender: Any) {
+        let url2 = URL (string: "https://www.eshakti.com")!
+        UIApplication.shared.open (url2)
+    }
+    
+    @IBAction func christianButton(_ sender: Any) {
+        let url3 = URL (string: "https://christianomeshun.com")!
+        UIApplication.shared.open (url3)
+    }
+    
+    @IBAction func altarButton(_ sender: Any) {
+        let url4 = URL (string: "https://www.altarpdx.com")!
+        UIApplication.shared.open (url4)
+    }
+    
+    @IBAction func curveButton(_ sender: Any) {
+        let url5 = URL (string: "https://curvegirl.com")!
+        UIApplication.shared.open (url5)
+    }
+    
+    @IBAction func copperButton(_ sender: Any) {
+        let url6 = URL (string: "https://copperunionapparel.com")!
+        UIApplication.shared.open (url6)
+    }
+    
+    @IBAction func goodButton(_ sender: Any) {
+        let url7 = URL (string: "https://www.goodamerican.com")!
+        UIApplication.shared.open (url7)
+    }
+    
+    @IBAction func loudButton(_ sender: Any) {
+        let url8 = URL (string: "https://loudbodies.com")!
+        UIApplication.shared.open (url8)
+    }
+    
+    @IBAction func alphineButton(_ sender: Any) {
+        let url9 = URL (string: "https://alpinebutterflyswim.com")!
+        UIApplication.shared.open (url9)
+    }
+    
+    @IBAction func arqButton(_ sender: Any) {
+        let url10 = URL (string: "https://shoparq.com")!
+        UIApplication.shared.open (url10)
     }
     
     /*
